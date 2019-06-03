b81e0e3b32778821a1423a47f0d347b74a091ffb
Adam Vranay <darkadamis@gmail.com>
2019-06-03 05:11:43 +0200

    v2

--- uloha06_1.sh
+++ uloha06_1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-join -t , -j 1 <(cat social.txt | sort | sed s/$/,/) <(cat actor.txt | sort | sed s/$/,/) | cut -d , -f 1
+join -t , -j 1 <(cat social.txt | sort | sed s/$/,/) <(cat actor.txt | sort | sed s/$/,/) | cut -d , -f 1,2 >tmp
+join -t , -j 1 <(cat beverly_hills.txt | sort | sed s/$/,/) tmp | cut -d , -f 1
+rm tmp
